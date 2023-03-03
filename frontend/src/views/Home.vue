9ee8541b4f257d09b636131c9496b46d6722a553
Andy Fleming <andy@ulendo.com>
2023-03-03 10:28:46 +0200

    Update

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { onCreated } from 'vue'
+
+  onCreated(() => {
+    console.log(`the component is now mounted.`)
+  })
+</script>
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col class="" cols="12">
+        <v-card>
+        <v-card-title>Charges</v-card-title>
+        <v-card-text>
+
+        </v-card-text>
+      </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
